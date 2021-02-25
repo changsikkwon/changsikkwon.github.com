cd4a40d8f323552c3f2bf6d449b7a88ae80d3513
chang sik kwon <kcs1987@gamil.com>
2021-02-25 12:21:06 +0900

    blog

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -23,6 +23,30 @@ var store = [{
         "url": "/%EC%9E%90%EB%A3%8C%EA%B5%AC%EC%A1%B0/data%20structure/array/",
         "teaser": null
       },{
+        "title": "Set, Dictionary, Hash",
+        "excerpt":"Set   set는 list처럼 순열 자료구조이다. 하지만 set는 순서라는 개념이 존재하지 않는다.   특징      데이터를 비순차적으로 저장할 수 있는 순열 자료구조이다.   삽입 순서대로 저장되지 않는다.   수정가능하다.   동일한 값을 여러번 삽입 불가능하다. 동일한값이 여러번 삽입 되면 하나의 값만 저장된다.   Fast Lookup이 필요할 때 주로 쓰인다.   구조         set요소들이 저장될 때 순서는 다음과 같다.            저장할 요소읙 값의 hash 값을 구한다.       hash값에 해당하는 공간(bucket)에 값을 저장한다.           이렇게 set는 저장하고자 하는 값의 해쉬값에 해당하는 bucket에 값을 저장하기 때문에 순서가 없다. 순서가 없기 때문에 indexing도 없다.   hash값을 기반으로 저장하기 때문에 look up이 굉장히 빠르다.   Dictionary   Dictionary는 key-value 형태의 값을 저장할 수 있는 자료구조이다.   특징      set와 마찬가지로 특정 순서대로 데이터를 리턴하지 않는다.   key의 값은 중복될수 없다.(만일 중복된 key가 있으면 먼저 있던 key와 value를 대체한다.)   수정 가능하다.   구조         set와 비슷하게 key값의 hash값을 구한 후 hash값에 속해있는 bucket에 값을 저장한다.   Hash   hash란 단 방향 암호화이다. 입력 데이터를 변환하여 원본 데이터로 복호화할 수 없도록 하는 과정이다.  ","categories": ["자료구조","Data Structure"],
+        "tags": ["자료구조","Data Structure"],
+        "url": "/%EC%9E%90%EB%A3%8C%EA%B5%AC%EC%A1%B0/data%20structure/set-dictionary-hash/",
+        "teaser": null
+      },{
+        "title": "Stack & Queue",
+        "excerpt":"Stack   stack은 LIFO(Last In First Out)라고 한다. 마지막으로 저장한 데이터가 처음으로 읽힌다는 뜻이다.         Stack에서 데이터 저장은 push라고 한다.   데이터를 읽어들이는 건 pop라고 한다. 다만 pop은 읽어들임과 동시에 stack에서 삭제한다.   리스트를 사용한 Stack 구현 예제   class Stack:    def __init__(self):      self._stack = []     def push(self, data):      self._stack.append(data)     def pop(self):      if len(self._stack) == 0:        return None       data = self._stack[-1]      del self._stack[-1]       return data     def peek(self):      if len(self._stack) == 0:        return None       data = self._stack[-1]       return data   Queue   Stack과 반대로 FIFO(First In First Out)이다. 즉 먼저 push 된게 먼저 pop된다는 말이다.      리스트를 사용한 Queue 구현 예제   class Queue:     def __init__(self):         self._queue = []      def push(self, data):         return self._queue.append(data)      def pop(self)         if len(self._queue) == 0:             return None          return self._queue.pop()      def peek(self):         if len(self._queue) == 0:             return None          return self[0]  ","categories": ["자료구조","Data Structure"],
+        "tags": ["자료구조","Data Structure"],
+        "url": "/%EC%9E%90%EB%A3%8C%EA%B5%AC%EC%A1%B0/data%20structure/stack-queue/",
+        "teaser": null
+      },{
+        "title": "Tree",
+        "excerpt":"Tree      Tree는 일반적으로 대상 정보의 각 항목들을 계층적으로 연관되도록 구조화 시키고자 할 때 사용하는 비선형 자료구조이다.   데이터 요소들의 단순한 나열이 아닌 부모 - 자식 관곅의 계층적 구조로 표현된다.   Tree는 그래프의 한 종류이며 사이클이 없다.   Tree 자료구조는 여러유형이 있지만 그 중 가장 기본은 binary tree 구조가 대표적이다.   binary tree는 두개의 자식노드를 가진 트리 형태이다.   Tree의 자료구조는 데이터를 거꾸로된 나무 형태로 저장하는 모양이다.   계층적인 관계의 표현에 쓰이고, 윈도우와 리눅스의 파일시스템 구조도 Tree로 표현된다.   용어         Node : Tree구조의 교점이다. Node가 데이터를 가지고 있고 또한 자식노드를 가지고 있다. Tree 자료구조는 노드를 기본으로 구성된다.   Root Node: Tree구조의 가장 위 노드.   Edge: Tree를 구성하기 위해 노드와 노드를 연결하는 선이다.   level: Tree의 특정 깊이를 가지는 노드의 집합이다.   degree: 하위 Tree개수 / 각 노드가 지닌 가지의 수를 말한다.   Internal Node : Leaf노드를 제외한 중간에 위치한 노드들을 말한다.   Leaf Node: 하위에 다른 노드가 연결되어 있지 않은 노드이다.   Tree의 속성 중 가장 중요한 것이 ‘루트 노드를 제외한 모든 노드는 단 하나의 부모노드만을 가진다’는 것이다.   Tree의 탐색   Tree의 순회란 Tree의 각 노드를 체계적인 방법으로 방문하는 과정을 말한다.      전위순회(preorder)루트노드 - 왼쪽 서브Tree - 오른쪽 서브Tree 순으로 순회하는 방식이다.   중위순회(inorder)루트노드에서 시작해서 왼쪽 서브Tree - 노드 - 오른쪽 서브Tree 순으로 순회하는 방식이다.   후위순회(postorder)루트노드에서 시작해서 왼쪽 서브Tree - 오른쪽 서브Tree - 노드 순으로 순회하는 방식이다.  ","categories": ["자료구조","Data Structure"],
+        "tags": ["자료구조","Data Structure"],
+        "url": "/%EC%9E%90%EB%A3%8C%EA%B5%AC%EC%A1%B0/data%20structure/tree/",
+        "teaser": null
+      },{
+        "title": "Tuple",
+        "excerpt":"특징      List와 마찬가지로 데이터를 순차적으로 저장할 수 있는 순열 자료구조이다.   LIst와 다르게 한 번 정의되고 나면 수정할 수 없다.   2-3개 정도의 적은 수의 소규모 데이터를 저장할 때 많이 사용한다.   장점      List보다 더 가볍고 메모리를 적게 차지한다.   Tuple은 간단한 값을 빨리 표현하고 싶을때 많이 사용한다.            ex           // Tuple을 사용하는 경우 [(1,2), (2,4)] // Array(List) 안의 Tuple  // Tuple을 안 쓰는 경우에는 class를 생성해야함 class cord: \tdef __init__(self, x, y): \t\tself.x = x \t\tself.y = y   단점      Tuple의 데이터는 무슨 의미인지 명확하지 않다.   데이터의 의미를 문맥을 보고 가정해야한다.            예를들어 객체의 경우 key-value 쌍으로 이루어진 데이터이기 때문에 무슨 데이터인지 파악이 쉽지만, Tuple의 경우 괄호 안에 데이터만 담겨있기 때문에 문맥에 맞게 의미를 추측해야 한다.           Tuple을 사용하기 좋은 곳           List를 쓰기에는 간단한 데이터들을 표현할 때 사용한다.              ex) 좌표 데이터           coordinations = [     (1, 2),     (3, 4),     (5, 6) ]          ","categories": ["자료구조","Data Structure"],
+        "tags": ["자료구조","Data Structure"],
+        "url": "/%EC%9E%90%EB%A3%8C%EA%B5%AC%EC%A1%B0/data%20structure/tuple/",
+        "teaser": null
+      },{
         "title": "자료구조란?",
         "excerpt":"   자료구조란 데이터에 편리하게 접근하고 조작하기 위한 데이터를 저장하거나 조직하는 방법이다.   자료구조의 종류에는 여라가지가 있습니다. 하지만 모든 목적에 부합하는 자료구조는 없다. 따라서 각각의 자료구조가 갖는 장점과 한계를 잘 이해하고 상황에 맞게 올바른 자료구조를 선택하고 사용하는 것이 중요하다.   자료구조는 언어별로 지원하는 양상이 다르다.   각 언어가 가진 자료구조의 종류와 그것에 대한 사용 방법을 익히는 것이 중요하지만, 무엇보다 각 자료구조의 본질과 컨셉을 이해하고 상황에 맞는 적절한 자료 구조를 선택하는 것이 중요하다.   자료구조의 분류                  Primitive Data Structure(단순 구조)       프로그래밍에서 사용되는 기본 데이터 타입                       None-Primitive Data Structure(비단순 구조)       단순한 데이터를 저장하는 구조가 아니라 여러 데이터를 목적에 맞게 효과적으로 저장하는 자료 구조 - Linear Data Structure(선형 구조)       저장되는 자료의 전후 관계가 1:1 (ex. List, Stacks, Queues) - Non-Linear Data Structure(비선형 구조)       데이터 항목 사이의 관계가 1:n 또는 n:m (ex. Graphs, Trees )          ","categories": ["자료구조","Data Structure"],
         "tags": ["자료구조","Data Structure"],
